8e388b466fb7090c4f1d0b63f7178f20bdc8b88e
BlakeRude <46767785+bdRude@users.noreply.github.com>
2020-05-11 12:29:55 -0700

    ...

--- Semester Project/OLDbox.php
+++ Semester Project/OLDbox.php
@@ -3,7 +3,7 @@
 //========== Global Parameters ==========
 
 $msgIndex = 0;
-$i = 0;
+
 $targetDB = '';
 $querytype = 'sql';
 $inputQuery = '';
@@ -14,10 +14,7 @@ $selection = '';
 $errorMsg = array('');
 $successMsg = array('');
 $defaultTables = ['information_schema', 'mysql', 'performance_schema', 'sakila', 'sys', 'world'];
-$selectflag = 0;
-$whereflag = 0;
-$fromflag = 0;
-$parencount = 0;
+
 //========== Database Connection ==========
 
 $servername = "localhost";
@@ -48,20 +45,6 @@ if(isset($_POST['submit']))
   }
 
   $search_result = null;
-  $isRA = false;
-
-  $numSymbols = 0;
-  $numLParen = 0;
-  $numRParen = 0;
-  $numParen = 0;
-  $numParenSymbols = 0;
-  $succflag = 1;
-  $s = strtoupper('select');
-  $f = strtoupper('from');
-  $w = strtoupper('where');
-  $j = 0;
-  //$outmsg = null;
-  $temp = 0;
 
   $inputQuery = trim($_POST['inputQuery']);
  
@@ -74,125 +57,44 @@ if(isset($_POST['submit']))
       //updateMessages('error', 'Database deletion not allowed on this platform.');
   }
   // else
-  
-    
-  for($i = 0; $i < strlen($inputQuery); $i++)
   {
-    if( (($inputQuery[$i] == 'S') && ($inputQuery[$i+1] == 'e') && ($inputQuery[$i+2] == 'l') && ($inputQuery[$i+3] == 'e')) || 
-        (($inputQuery[$i] == 'P') && ($inputQuery[$i+1] == 'r') && ($inputQuery[$i+2] == 'o') && ($inputQuery[$i+3] == 'j')) ||
-        (($inputQuery[$i] == 'U') && ($inputQuery[$i+1] == 'N') && ($inputQuery[$i+2] == 'I') && ($inputQuery[$i+3] == 'O')) ||
-        (($inputQuery[$i] == 'I') && ($inputQuery[$i+1] == 'N') && ($inputQuery[$i+2] == 'T') && ($inputQuery[$i+3] == 'E')) ||
-        (($inputQuery[$i] == 'D') && ($inputQuery[$i+1] == 'I') && ($inputQuery[$i+2] == 'F') && ($inputQuery[$i+3] == 'F')) ||
-        $inputQuery[$i] == 'X' ||
-        (($inputQuery[$i] == 'D') && ($inputQuery[$i+1] == 'I') && ($inputQuery[$i+2] == 'V') && ($inputQuery[$i+3] == 'I'))||
-        (($inputQuery[$i] == 'J') && ($inputQuery[$i+1] == 'O') && ($inputQuery[$i+2] == 'I') && ($inputQuery[$i+3] == 'N'))||
-        (($inputQuery[$i] == 'A') && ($inputQuery[$i+1] == 'N') && ($inputQuery[$i+2] == 'D')) ||
-        (($inputQuery[$i] == 'O') && ($inputQuery[$i+1] == 'R')) ||
-        (($inputQuery[$i] == 'N') && ($inputQuery[$i+1] == 'O') && ($inputQuery[$i+2] == 'T')))
     {
-      if(
-        (($inputQuery[$i] == 'S') && ($inputQuery[$i+1] == 'e') && ($inputQuery[$i+2] == 'l') && ($inputQuery[$i+3] == 'e')) ||
-        (($inputQuery[$i] == 'P') && ($inputQuery[$i+1] == 'r') && ($inputQuery[$i+2] == 'o') && ($inputQuery[$i+3] == 'j')))
-        //(($inputQuery[$i] == 'J') && ($inputQuery[$i+1] == 'O') && ($inputQuery[$i+2] == 'I') && ($inputQuery[$i+3] == 'N'))
-      {
-        $numSymbols++;;
-        $numParenSymbols++;
-      }else{
-        $numSymbols++;
-      }
-    } elseif ($inputQuery[$i] == '(') {
-        $numLParen++;
-    } elseif ($inputQuery[$i] == ')') {
-        $numRParen++;
-    } else {
-        //nothing
+      if (0)//(mysqli_multi_query($conn, str_replace('<br>', '', $inputQuery)))
+    {
+        do
+        {   
+          
+            //check first result
+            if ($result = mysqli_store_result())
+            {
+                $search_result = $result; echo $inputQuery;
+                //free the result and move on to next query
+                mysqli_free_result($result);
+            }
+            else
+            {
+                updateMessages('error', $conn->error);
+            }
+
+            $success = mysqli_next_result($conn); echo $success;
+            if (!$success)
+            {
+                updateMessages('error', $conn->error);
+            }
+            else 
+            {
+                $search_result = mysqli_store_result($conn);
+            }
+        }
+        while ($success);
     }
-  }
-  if($numLParen == $numRParen)
-  {
-    $numParen = $numLParen + $numRParen;
-  }
 
- if($numParen > (2 * $numParenSymbols))
- {
-  updateMessages('error', 'Too Many Parenthesis: Error');
-  $succflag = 0;
- } elseif($numParen < (2 * $numParenSymbols)){
-  updateMessages('error', 'Too Few Parenthesis: Error');
-  $succflag = 0;
- }
-
-  if($succflag)
-  {
-    //Display Symbol Numbers and Parenthesis.
-    //updateMessages('success', 'There are '.$numSymbols.' RA symbols and '.$numParen.' Parenthesis.');
-    $parencount = 0;
-    for($i = 0; $i < strlen($inputQuery); $i++)
-    {
-      //Projection Check
-      if( (($inputQuery[$i] == 'P') && ($inputQuery[$i+1] == 'r') && ($inputQuery[$i+2] == 'o') && ($inputQuery[$i+3] == 'j')) )
+      //$search_result = mysqli_store_result($conn);
+      $search_result = $conn->query($inputQuery);
+      if(is_bool($search_result) and $search_result)
       {
-        $i = $i + 10;
-        if($inputQuery[$i] == '(')
-        {
-          updateMessages('success', $s.' *');
-        } elseif($inputQuery[$i] != '('){
-          $temp = $i;
-          $j = 0;
-          while ($inputQuery[$i] != '(') {
-            $j++;
-            $i++;
-          }
-          $parencount++;
-          $outmsg = substr($inputQuery, $temp, $j);
-          $selectflag=1;
-          updateMessages('success', $s.' '.$outmsg);
-          $selectflag=0;
-        }
-
-      }
-
-      //Selection Check
-      if( (($inputQuery[$i] == 'S') && ($inputQuery[$i+1] == 'e') && ($inputQuery[$i+2] == 'l') && ($inputQuery[$i+3] == 'e')) )
-      {
-        $i = $i + 9;
-        if($inputQuery[$i] == '(')
-        {
-          updateMessages('success', $w.' *');
-        } elseif($inputQuery[$i] != '('){
-          $temp = $i;
-          $j = 0;
-          while ($inputQuery[$i] != '(') {
-            $j++;
-            $i++;
-          }
-          $parencount++;
-          $outmsg = substr($inputQuery, $temp, $j);
-          $whereflag = 1;
-          updateMessages('success', $w.' '.$outmsg);
-          $whereflag = 0;
-        }
-      }
-
-      //From vars
-      if( $inputQuery[$i] == '(' )
-      {
-        if($parencount == $numLParen)
-        {
-          if($inputQuery[$i] == ')')
-          {
-            updateMessages('success', $f.' *');
-          } elseif($inputQuery[$i] != ')'){
-            $temp = $i;
-            $j = 0;
-            while ($inputQuery[$i] != ')') {
-             $j++;
-              $i++;
-            }
-            $outmsg = substr($inputQuery, $temp+1, $j-1);
-            updateMessages('success', $f.' '.$outmsg);
-            }
-        }
+        $operation = substr($inputQuery, 0, strpos($inputQuery, ' '));
+        updateMessages('success', ucfirst($operation).' operation successfully executed.');
       }
     }
   }
@@ -254,10 +156,8 @@ function updateMessages($msgStatus, $msg)
     if($msg != '')
     {
         $msgIndex += 1;
-
         if ($msgStatus == 'success') {array_push($successMsg, $msgIndex.'. '.$msg);}
         else {array_push($errorMsg, $msgIndex.'. '.$msg);}
-        $msgIndex = $tempo+1;
     }
 }
 
@@ -267,7 +167,6 @@ function updateMessages($msgStatus, $msg)
 
 <!DOCTYPE html>
 <html>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <script src = "print.js"></script>
     <head>
         <title>Relational Algebra Interpreter for SQL</title>
@@ -344,20 +243,42 @@ function updateMessages($msgStatus, $msg)
                 <textarea class = "FormElement" name = "inputQuery" id = "input" cols = "40" rows = "10" placeholder = "Type Query Here"><?php echo $inputQuery; ?></textarea>
 
                 <br>
-                <button type = "button" alt="Selection" width="20" height="20" onclick = "printRA('input','Selection()')"> <img src = "selection.png"> </button>
-                <button type = "button" alt="Projection" width="20" height="20" onclick = "printRA('input','Projection()')"> <img src = "projection.png"> </button>
-                <button type = "button" alt="Union" width="20" height="20" onclick = "printRA('input','UNION')"> <img src = "union.png"> </button>
-                <button type = "button" alt="Intersection" width="20" height="20" onclick = "printRA('input','INTERSECTION')"> <img src = "intersection.png"> </button>
-                <button type = "button" alt="Difference" width="20" height="20" onclick = "printRA('input','DIFF')"> <img src = "diff.png"> </button>
-                <button type = "button" alt="Cartesian" width="20" height="20" onclick = "printRA('input','X')"> <img src = "cartesian.png"> </button>
-                <button type = "button" alt="Divide" width="20" height="20" onclick = "printRA('input','DIVIDE')"> <img src = "divide.png"> </button>
-                <button type = "button" alt="Join" width="31" height="33" onclick = "printRA('input','JOIN')"> <img src = "join.png"> </button>
-                <button type = "button" alt="And" width="20" height="20" onclick = "printRA('input','AND')"> <img src = "and.png"> </button>
-                <button type = "button" alt="Or" width="20" height="20" onclick = "printRA('input','OR')"> <img src = "or.png"> </button>
-                <button type = "button" alt="Not" width="20" height="20" onclick = "printRA('input','NOT')"> <img src = "not.png"> </button>
+                <button type = "button" alt="Selection" width="20" height="20" onclick = "printRA('input','σ()')"> <img src = "selection.png"> </button>
+                <button type = "button" alt="Projection" width="20" height="20" onclick = "printRA('input','Π()')"> <img src = "projection.png"> </button>
+                <button type = "button" alt="Union" width="20" height="20" onclick = "printRA('input','∪')"> <img src = "union.png"> </button>
+                <button type = "button" alt="Intersection" width="20" height="20" onclick = "printRA('input','∩')"> <img src = "intersection.png"> </button>
+                <button type = "button" alt="Difference" width="20" height="20" onclick = "printRA('input','-')"> <img src = "diff.png"> </button>
+                <button type = "button" alt="Cartesian" width="20" height="20" onclick = "printRA('input','×')"> <img src = "cartesian.png"> </button>
+                <button type = "button" alt="Divide" width="20" height="20" onclick = "printRA('input','÷')"> <img src = "divide.png"> </button>
+                <button type = "button" alt="Join" width="31" height="33" onclick = "printRA('input','⨝()')"> <img src = "join.png"> </button>
+                <button type = "button" alt="And" width="20" height="20" onclick = "printRA('input','∧')"> <img src = "and.png"> </button>
+                <button type = "button" alt="Or" width="20" height="20" onclick = "printRA('input','∨')"> <img src = "or.png"> </button>
+                <button type = "button" alt="Not" width="20" height="20" onclick = "printRA('input','˜')"> <img src = "not.png"> </button>
                 <br>
                 <input type = "submit" id = "submit" name = "submit" value = "Submit" onclick = "return checkInput();">
 
+          </fieldset>
+        </section>
+      </form>
+
+      <!-- Relational SECTION -->
+      <form action = "sandbox.php" method = "post">
+
+        <section class = "block-of-text">
+          <fieldset>
+            <legend>Relational Algebra</legend>
+
+              <?php $messages = array_merge($successMsg, $errorMsg); asort($messages); ?>
+                <?php foreach ($messages as $msg):?>
+                    <b><?php if ($msg !== '') { echo $msg.'<br>';} ?></b>
+                <?php endforeach; ?>
+
+                <br>
+
+                <?php if($search_result and !is_bool($search_result)): ?>
+
+                <?php endif?>
+                
           </fieldset>
         </section>
       </form>
